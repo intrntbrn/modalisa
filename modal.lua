7e9e41a8e48a851b8579f73aa9198b49ed512df7
BZ <b@zimmer.bz>
2023-10-18 23:20:08 +0200

    trunner: fix all issues

--- modal.lua
+++ modal.lua
@@ -184,11 +184,10 @@ end
 
 local trunner = {}
 function trunner:new(t, root_key)
-	local opts = t:opts()
-
-	self.mm = mmodmap(root_key.key or "", root_key.mods or {})
+	self.mm = mmodmap(root_key.key or "", root_key.mods or {}, mod_conversion)
 	self.ran_once = false
 	self.tree = t
+	self.keygrabber = nil
 
 	if vim.tbl_count(t:successors()) == 0 then
 		-- the tree is empty and there is nothing to do, except to wait for the
@@ -204,6 +203,30 @@ function trunner:new(t, root_key)
 	end
 
 	self:set_tree(t)
+	local grabber = self:make_keygrabber(t)
+	self.keygrabber = grabber
+
+	global_keygrabber = grabber
+	global_execute = function(...)
+		local ret = self.press_key(...)
+
+		-- HACK:
+		-- force update after clicks
+		require("gears").timer({
+			timeout = 0.05,
+			callback = function()
+				if global_keygrabber then
+					on_update(t)
+				end
+			end,
+			autostart = true,
+			single_shot = true,
+		})
+
+		return ret
+	end
+
+	grabber:start()
 
 	return self
 end
@@ -235,6 +258,150 @@ function trunner:run(t)
 	return nil
 end
 
+function trunner:keypressed_callback()
+	return function(_, modifiers, key)
+		-- filter mods that are ignored by default (capslock, numlock)
+		local filtered_modifiers = {}
+		for _, m in ipairs(modifiers) do
+			local ignore = vim.tbl_contains(ignore_mods, m)
+			if not ignore then
+				table.insert(filtered_modifiers, m)
+			end
+		end
+		modifiers = filtered_modifiers
+
+		print("pressed callback: ", dump(modifiers), dump(key))
+
+		local converted_key = mod_conversion[key]
+		self.mm:press(key, modifiers)
+
+		local keys = self.keybinds[key]
+		if keys then
+			local function accept_key(v)
+				-- stop key
+				if v.stop then
+					self.keygrabber:stop()
+					return
+				end
+
+				-- back key
+				if v.back then
+					self:press_key("back", self.keygrabber)
+					return
+				end
+
+				-- regular key
+				self:press_key(v.name, self.keygrabber)
+			end
+
+			local function is_match(v, comparator)
+				if not (#v == #comparator) then
+					return false
+				end
+				local mod = {}
+				for _, v2 in ipairs(v) do
+					mod[v2] = true
+				end
+				local match = true
+				for _, v2 in ipairs(comparator) do
+					match = match and mod[v2]
+				end
+				return match
+			end
+
+			-- check exact match first to skip combination calculations
+			for _, v in pairs(keys) do
+				if is_match(v.mods, modifiers) then
+					return accept_key(v)
+				end
+			end
+
+			if self.mm:has_pressed_mods() then -- TODO:
+				-- find the match with the least amount of ignored mods
+				local pressed_mods_list = self.mm:get_pressed_mods()
+				local combinations = {}
+				for combo in util.unique_combinations(pressed_mods_list) do
+					table.insert(combinations, combo)
+				end
+				table.sort(combinations, function(a, b)
+					return #a < #b
+				end)
+
+				-- combinations are sorted by mod count ascending
+				for _, combi in ipairs(combinations) do
+					-- keys are sorted by mod count descending
+					for _, v in pairs(keys) do
+						local filtered = {} -- modifiers - combi
+						for _, mod in ipairs(modifiers) do
+							if not vim.tbl_contains(combi, mod) then
+								table.insert(filtered, mod)
+							end
+						end
+						-- v.mods == modifiers - combi?
+						if is_match(v.mods, filtered) then
+							return accept_key(v)
+						end
+					end
+				end
+			end
+		end
+		-- no match!
+
+		if converted_key then
+			-- user is pressing a mod key, we have to ignore it
+			return
+		end
+
+		-- key is not defined and not a mod
+		if self.tree:opts().stop_on_unknown_key then
+			print("unknown key: ", key)
+			self.keygrabber:stop()
+		end
+	end
+end
+
+function trunner:keyreleased_callback()
+	return function(_, _, key)
+		self.mm:release(key)
+		if self.mm:has_pressed_mods() then
+			return
+		end
+
+		local opts = self.tree:opts()
+
+		if opts.stay_open then
+			return
+		end
+
+		local mod_release = opts.mod_release_stop
+		if mod_release == "always" or self.ran_once and mod_release == "after" then
+			print("mm: all mods are released: ", mod_release)
+			self.keygrabber:stop()
+			return
+		end
+		print("mm: all mods are released: ", mod_release)
+	end
+end
+
+function trunner:make_keygrabber(t)
+	local grabber = akeygrabber({
+		keyreleased_callback = self:keyreleased_callback(),
+		keypressed_callback = self:keypressed_callback(),
+		-- timeout = opts.timeout and opts.timeout > 0 and opts.timeout / 1000,
+		-- timeout_callback = function()
+		-- 	run(t)
+		-- 	on_stop(t)
+		-- end,
+		start_callback = function()
+			on_start(t)
+		end,
+		stop_callback = function()
+			on_stop(t)
+		end,
+	})
+	return grabber
+end
+
 function trunner:traverse(tree, key)
 	local next_tree = tree[key]
 
@@ -253,14 +420,62 @@ function trunner:traverse(tree, key)
 	return self:run(next_tree)
 end
 
+function trunner:press_key(key, grabber, force_continue)
+	-- special key hacks
+	if key == "back" then
+		local prev = self.tree:pred()
+		if prev then
+			self:set_tree(prev)
+		end
+		return
+	end
+
+	if key == "stop" then
+		grabber = grabber or global_keygrabber
+		if grabber then
+			grabber:stop()
+		end
+		return
+	end
+
+	local next_tree = self:traverse(self.tree, key)
+	if next_tree then
+		self:set_tree(next_tree)
+		-- set_next_tree(next_tree)
+		return
+	end
+
+	-- no next tree
+
+	if force_continue then
+		return
+	end
+
+	if self.mm:has_pressed_mods() then
+		return
+	end
+
+	-- stop the grabber
+	grabber = grabber or global_keygrabber
+	if grabber then
+		grabber:stop()
+	end
+end
+
 local function grab(t, keybind)
 	assert(mod_conversion)
+
 	local opts = t:opts()
 
 	-- hold mod init
 	local hold_mod = opts.mod_hold_continue and keybind
 	local root_key = hold_mod and keybind
 
+	trunner:new(t, root_key)
+	if true then
+		return
+	end
+	--
 	if hold_mod then
 		-- TODO: send notification about how to setup keybinds
 		assert(root_key and root_key.key, "hold_mod is active but there is no root key")
@@ -380,7 +595,7 @@ local function grab(t, keybind)
 
 	local grabber = akeygrabber({
 		-- keyreleased_callback is only used for hold_mod detection
-		keyreleased_callback = hold_mod and function(self, _, key)
+		keyreleased_callback = function(self, _, key)
 			mm:release(key)
 			if mm:has_pressed_mods() then
 				return
