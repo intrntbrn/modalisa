eb5132b4b09a9fcd82e77679080d105f68ed9055
BZ <b@zimmer.bz>
2023-10-26 22:39:33 +0200

    refactor: move api back to init again

--- init.lua
+++ init.lua
@@ -1,21 +1,84 @@
 local M = {}
 
 local dump = require("motion.lib.vim").inspect
+local root_tree = require("motion.root")
+local modal = require("motion.modal")
+local config = require("motion.config")
+
+-- run key sequence on root
+function M.run(...)
+	return modal.run(...)
+end
+
+-- run inline tree
+function M.run_tree(...)
+	return modal.run_tree(...)
+end
+
+function M.stop()
+	print("STOP BUG PLS NO")
+end
+
+function M.fake_input(...)
+	return modal.fake_input(...)
+end
+
+-- add key to root
+function M.add_key(...)
+	return root_tree.add(...)
+end
+
+-- remove key from root
+function M.remove_key(...)
+	return root_tree.remove(...)
+end
+
+-- get key from root
+function M.get_tree(...)
+	return root_tree.get(...)
+end
+
+-- add keys to root
+function M.add_keys(...)
+	return root_tree.add_keys(...)
+end
+
+-- config set var
+function M.set(k, v)
+	config[k] = v
+end
+
+-- config get var
+function M.get(k)
+	return config[k]
+end
+
+-- get complete config
+function M.get_config()
+	return config.get()
+end
 
 local mt = {
 	__call = function(_, opts)
 		return M.setup(opts)
 	end,
-	__index = function(_, k)
-		return require("motion.api")[k]
-	end,
 }
+
+local function gen_signals()
+	for k, v in pairs(M) do
+		if type(v) == "function" then
+			local name = string.format("motion::%s", k)
+			awesome.connect_signal(name, function(...)
+				v(...)
+			end)
+		end
+	end
+end
 
 local once
 function M.setup(opts)
 	assert(once == nil, "motion is already setup")
 	once = true
-	local config = require("motion.config")
 	config.setup(opts)
 	opts = config.get() or {}
 
@@ -38,6 +101,8 @@ function M.setup(opts)
 	if pcall(require, "motion_keys") then
 		require("motion_keys").setup(opts)
 	end
+
+	gen_signals()
 
 	return setmetatable(M, mt)
 end
