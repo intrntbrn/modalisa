32fba626e8808ecc832d7a62a4ca66230fbe2c38
BZ <b@zimmer.bz>
2023-10-28 18:15:34 +0200

    improvement(preset): set metatable recursive

--- presets/metatable.lua
+++ presets/metatable.lua
@@ -2,16 +2,16 @@ local M = {}
 
 local mt = {
 	__add = function(lhs, rhs)
-		return vim.tbl_deep_extend("force", lhs, rhs)
+		return M.set(vim.tbl_deep_extend("force", lhs, rhs))
 	end,
 }
 
-local function set(obj)
+function M.set(obj)
 	return setmetatable(obj, mt)
 end
 
 return setmetatable(M, {
 	__call = function(_, obj)
-		return set(obj)
+		return M.set(obj)
 	end,
 })
